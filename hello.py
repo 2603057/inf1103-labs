1ed848f3171aeecdaf2229133fa6171affeb3943
MORDY35 <2603057@sit.singaporetech.edu.sg>
2026-09-04 15:09:42 +0800

    Interactive profile creator

--- hello.py
+++ hello.py
@@ -4,7 +4,9 @@ print("my first post!")
 print("================================")
 
 
-username = "cool_creator"
+username = input("Enter Username:")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
 bio = "Fun Blogger"
 followers = 100
 
@@ -17,6 +19,10 @@ print("Day 2:", followers)
 followers += 10
 print("Day 3:", followers)
 
-print("Username", username)
+print("\nInstagram Profile")
+print("=====================")
+print("Username: ", username)
+print("Age: ",age)
+print("category: ",category)
 print("Bio:", bio)
 print("Followers:",followers)
