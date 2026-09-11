85c06e57040a9187e94505be22c185ef47983339
2603057 <2603057@sit.singaporetech.edu.sg>
2026-09-11 17:58:37 +0800

    week 2 Labs/

--- week 2 Lab/inventory_management_auditor.py
+++ week 2 Lab/inventory_management_auditor.py
@@ -1,25 +1,37 @@
 inventory = []
 inventory.clear()
 inventorynumber = 0
+failedentries = 0
 
 quit = False
 while quit == False:
   try:
     amount = int(input("Enter the amount: "))
+    if amount <= 0:
+          print("Amount must be greater than 0.")
+          failedentries += 1
+          continue
     itemname = input("Enter the item name: ")
+    if not itemname:
+          print("Item name cannot be empty.")
+          failedentries += 1
+          continue
     item = (amount, itemname)
     inventory.append(item)
     inventorynumber += amount
     if inventorynumber > 500:
-      print("Inventory limit exceeded.")
+      print("Inventory limit exceeded. Must be less than 500 items total")
       quit = True
     else:
-        wantquit = input("Do you want to quit? (y/n): ").lower()
-        if wantquit == "y":
+        wantquit = input("Do you want to quit? (quit): ").lower()
+        if wantquit == "quit":
             quit = True
             print("Inventory List:", inventory)
+            print("Total number of items in inventory:", inventorynumber)
+            print("Number of failed entries:", failedentries)
         else:
            continue
   except ValueError:
     print("Invalid input. Please enter a valid amount in numbers.")
+    failedentries += 1
     continue
