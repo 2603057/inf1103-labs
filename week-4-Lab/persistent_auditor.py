0581dec86469416058c9e2b569aae0e0085ddd48
2603057 <2603057@sit.singaporetech.edu.sg>
2026-09-26 12:07:49 +0800

    load inventory as per lab instuctions image

--- week-4-Lab/persistent_auditor.py
+++ week-4-Lab/persistent_auditor.py
@@ -1,15 +1,20 @@
+from pathlib import Path
+
 def load_inventory():
     try:
-       with open("inventory.txt", "r") as file:
-        for line in file:
-            print(line.strip())
-        
-    except FileNotFoundError:
-            print("Inventory file not found. Starting with 0 inventory.")
-            return 0, []
-    
-    except ValueError:
-            print("Invalid data in inventory file. Starting with 0 inventory.")
-            return 0, []
+        file_path = Path(__file__).parent / "inventory.txt"
 
-load_inventory()
+        with open(file_path, "r") as file:
+            for line in file:
+                print(line.strip())
+
+    except FileNotFoundError:
+        print("Inventory file not found. Starting with 0 inventory.")
+        return 0
+
+    except ValueError:
+        print("Invalid data in inventory file. Starting with 0 inventory.")
+        return 0
+
+
+inventory = load_inventory()
