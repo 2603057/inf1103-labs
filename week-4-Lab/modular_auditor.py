82a42e6c6c3fc8c5025901ae1bf4cbbc35aa85d1
2603057 <2603057@sit.singaporetech.edu.sg>
2026-09-26 10:31:40 +0800

    Week 4 Lab file

---
+++ week-4-Lab/modular_auditor.py
@@ -0,0 +1,54 @@
+def get_valid_input():
+    while True:
+        user_input = input("Enter the amount ( or 'quit' to stop): ")
+
+        if user_input.lower() == 'quit':
+               return "quit"
+
+        try:  
+          amount = int(user_input)
+
+          if amount <= 0:
+             print("Amount must be greater than 0.")
+             return None
+               
+          return amount
+
+        except ValueError:
+               print("Invalid input. Please enter a valid amount in numbers.")
+               return None
+
+def process_delvery(current_total, new_value):
+    new_total = current_total + new_value
+    return new_total
+
+def calculate_tax(amount):
+    tax = amount * 0.10
+    return tax
+
+def generate_report(total_units, failed_attempts):
+    print("total deliveres Processed:", total_units)
+    print("Number of failed entries:", failed_attempts)
+
+
+deliveries_processed = 0
+inventorynumber = 0
+failedentries = 0
+
+while True:
+    amount = get_valid_input()
+    if amount == "quit":
+        break
+    if amount is None:
+        failedentries += 1
+        continue
+    inventorynumber = process_delvery(inventorynumber, amount)
+
+    tax = calculate_tax(amount)
+    deliveries_processed += 1
+
+    print("Delivery amount:", amount) 
+    print("Tax for this delivery:", tax)
+    print("Current total inventory:", inventorynumber)
+
+generate_report(deliveries_processed, failedentries)
